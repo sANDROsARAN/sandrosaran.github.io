7032257a9f7df0b11138e0fc61d326672d1b1363
SDroSar <sar8887@gmail.com>
2026-01-10 13:19:34 +1100

    back to react

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div class="row">
+        <div class="big-letters">img</div>
+        <div>txt</div>
+      </div>
+
+      <div class="row">
+        <div>text</div>
+        <div>img</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
